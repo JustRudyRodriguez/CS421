1471a0c86070dbc71bfa3ae5ed1c16774a90eed8
Andrew <malmsteadaa@gmail.com>
2020-11-21 15:09:10 -0800

    remoive line

--- parser.cpp
+++ parser.cpp
@@ -545,11 +545,12 @@ int main()
   fin.open(filename.c_str());
   if(fin.good()){//if the file is good run the parser.
     while (getline(fin, line)) {//while we can get a line from the text
-      if(line.length()==0)
+      if(line.length()==NULL)
       continue;
       cout<<line<<endl;
       split = istringstream(line);
       story();      //** calls the <story> to start parsing
+      cout  << endl;
     }
 
   }else
